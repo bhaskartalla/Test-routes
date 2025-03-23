9493c3adfbd0d309f360554431e8e8cb141e8e76
bb7talla@gmail.com <bhaskar@bhaskars-MacBook-Air.local>
2025-03-23 19:48:19 +0530

    Build issue fixed

--- src/state/hooks/useTodos.js
+++ src/state/hooks/useTodos.js
@@ -2,8 +2,17 @@ import { useContext } from 'react'
 import { TodosContext } from '../contexts/TodosContext'
 
 const useTodos = () => {
-  const { state, dispatch } = useContext(TodosContext)
-  return [state, dispatch]
+  try {
+    const context = useContext(TodosContext)
+    // Return default values if context is undefined (happens during SSR)
+    if (!context) {
+      return [{ loading: false, error: null, todos: [] }, () => {}]
+    }
+    return [context.state, context.dispatch]
+  } catch (error) {
+    // Fallback for SSR
+    return [{ loading: false, error: null, todos: [] }, () => {}]
+  }
 }
 
 export default useTodos
